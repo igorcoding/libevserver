3551eebf1458239e0e92e944822a103592f53662
igorcoding <igorcoding@gmail.com>
2015-11-25 17:38:13 +0300

    working on server stop

--- src/evserver.h
+++ src/evserver.h
@@ -4,6 +4,7 @@
 #include <ev.h>
 #include <stdint.h>
 #include <arpa/inet.h>
+#include <stdbool.h>
 
 #include "util.h"
 #include "platform.h"
@@ -12,13 +13,6 @@
 #  define EVSRV_DEFAULT_BUF_LEN 4096
 #endif
 
-
-typedef enum {
-    EVSRV_IDLE,
-    EVSRV_LISTENING,
-    EVSRV_ACCEPTING,
-    EVSRV_STOPPED
-} evsrv_state_t;
 
 typedef struct _evserver evserver;
 typedef struct _evserver_info evserver_info;
@@ -58,7 +52,15 @@ void evserver_listen(evserver* self);
 void evserver_accept(evserver* self);
 void evserver_notify_fork_child(evserver* self);
 void evserver_run(evserver* self);
+void evserver_stop(evserver* self);
 
+
+typedef enum {
+    EVSRV_IDLE,
+    EVSRV_LISTENING,
+    EVSRV_ACCEPTING,
+    EVSRV_STOPPED
+} evsrv_state_t;
 
 struct _evsrv {
     struct ev_loop* loop;
@@ -99,6 +101,7 @@ int evsrv_listen(evsrv* self);
 int evsrv_accept(evsrv* self);
 void evsrv_notify_fork_child(evsrv* self);
 void evsrv_run(evsrv* self);
+void evsrv_stop(evsrv* self);
 
 
 
@@ -108,6 +111,15 @@ struct _evsrv_conn_info {
 };
 
 struct _evsrv_conn {
+    enum {
+        EVSRV_CONN_CREATED,
+        EVSRV_CONN_ACTIVE,
+        EVSRV_CONN_SHUTDOWN,
+        EVSRV_CONN_CLOSING,
+        EVSRV_CONN_CLOSING_FORCE,
+        EVSRV_CONN_STOPPED,
+        EVSRV_CONN_CLOSED
+    } state;
     evsrv* srv;
     evsrv_conn_info* info;
 
@@ -143,6 +155,12 @@ void evsrv_write(evsrv_conn* conn, const char* buf, size_t len);
 #define evsrv_stop_timer(loop, ev) do { \
     if (ev_is_active(ev)) { \
         ev_timer_stop(loop, ev); \
+    } \
+} while (0)
+
+#define evsrv_stop_io(loop, ev) do { \
+    if (ev_is_active(ev)) { \
+        ev_io_stop(loop, ev); \
     } \
 } while (0)
 
